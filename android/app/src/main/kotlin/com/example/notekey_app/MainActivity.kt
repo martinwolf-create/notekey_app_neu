c2c9c98e241d1b3730abacc7f23239b5f42b5230
martinwolf-create <wolfmartin707@gmail.com>
2025-08-05 09:44:57 +0200

    notekey_app_MW

---
+++ android/app/src/main/kotlin/com/example/notekey_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notekey_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'startscreen/startscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NOTEkey App',
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF0E7DE),
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
