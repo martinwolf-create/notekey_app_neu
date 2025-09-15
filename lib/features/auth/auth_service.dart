1cf2df263e0bd7acc56a1673d54fb61b947bb7bc
martinwolf-create <wolfmartin707@gmail.com>
2025-09-15 21:18:19 +0200

    auth_........

---
+++ lib/features/auth/auth_service.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<User?> signInWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    final cred = await _auth.signInWithEmailAndPassword(
+      email: email.trim(),
+      password: password,
+    );
+    return cred.user;
+  }
+
+  Future<void> sendPasswordReset(String email) async {
+    await _auth.sendPasswordResetEmail(email: email.trim());
+  }
+
+  Future<void> signOut() => _auth.signOut();
+}
